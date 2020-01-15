29369d7d83314aae7b687d2293833958ee9b1dd1
Thomas Goodman <tommysdevelopment@gmail.com>
2020-01-14 23:43:22 -0800

    Changed to yellow, added "CAMO"

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
-      home: MyHomePage(title: 'Jo Cabinet Register'),
+      home: MyHomePage(title: 'CAMO Jo Cabinet Register'),
     );
   }
 }
